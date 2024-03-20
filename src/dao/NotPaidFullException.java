66a72d8435cae401603b8fc5b372ddb2d99880e8
Nabin Ale <nale@wiley.com>
2024-03-20 15:11:18 +0000

    Not paid full exception

---
+++ src/dao/NotPaidFullException.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class NotPaidFullException {
+
+    public NotPaidFullException(String message) {
+        super(message);
+    }
+
+    public NotPaidFullException(String message, Throwable cause) {super(message, cause);}
+}
