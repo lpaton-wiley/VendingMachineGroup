1d6232e06eae8c71bb6b52b9a7eeddbac19cc7e8
Nabin Ale <nale@wiley.com>
2024-03-20 14:55:55 +0000

    Made changes to change

--- src/dto/Change.java
+++ src/dto/Change.java
@@ -1,4 +1,15 @@
 package dto;
 
-public class Change {
+public enum Change {
+    PENNY(1), NICKLE(5), DIME(10), QUARTER(25);
+
+    private int coinValue;
+
+    private Change(int denomination){
+        this.coinValue = denomination;
+    }
+
+    public int getDenomination(){
+        return coinValue;
+    }
 }
